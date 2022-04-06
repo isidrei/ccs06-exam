5f7c57760d318898c642378f3a378b70a4c9329d
isidrei <musni.danisidrei@auf.edu.ph>
2022-04-06 22:23:00 +0800

    Finished the First and Second Page Currently doing the third page

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -11,7 +11,7 @@ class GradeController extends Controller
        return view ('begin');
    }
 
-   public function enterGrades()
+   public function enterGrades(Request $request)
    {
     $student_1=$request->name_1;
     $student_2=$request->name_2;
@@ -19,7 +19,7 @@ class GradeController extends Controller
     $student_4=$request->name_4;
     $student_5=$request->name_5;
 
-    return view ('grades',[
+    return view ('enter-grades',[
         'student_1' => $student_1,
         'student_2' => $student_2,
         'student_3' => $student_3,
@@ -33,35 +33,56 @@ class GradeController extends Controller
        $average = ($score1 + $score2 + $score3) /3;
        return round($average,2);
    }
-   public function computePower(Request $request)
+   public function computeGrades(Request $request)
    {
-       $player_1 = $request->player_1;
-       $player_2 = $request->player_2;
-       $player_3 = $request->player_3;
+    $student_1=$request->name_1;
+    $student_2=$request->name_2;
+    $student_3=$request->name_3;
+    $student_4=$request->name_4;
+    $student_5=$request->name_5;
 
-       $p1_average = $this->computeAverageScore($request->p1_attempt1, $request->p1_attempt2, $request->p1_attempt3);
-       $p2_average = $this->computeAverageScore($request->p2_attempt1, $request->p2_attempt2, $request->p2_attempt3);
-       $p3_average = $this->computeAverageScore($request->p3_attempt1, $request->p3_attempt2, $request->p3_attempt3);
+       $s1_average = $this->computeAverageScore($request->s1_attempt1, $request->s1_attempt2);
+       $s2_average = $this->computeAverageScore($request->s2_attempt1, $request->s2_attempt2 );
+       $s3_average = $this->computeAverageScore($request->s3_attempt1, $request->s3_attempt2 );
+       $s4_average = $this->computeAverageScore($request->s4_attempt1, $request->s4_attempt2 );
+       $s5_average = $this->computeAverageScore($request->s5_attempt1, $request->s5_attempt2 );
 
-       return view('scores', [
-           'player_1' => $player_1,
-           'player_2' => $player_2,
-           'player_3' => $player_3,
+       return view('compute-grades', [
+        'student_1' => $student_1,
+        'student_2' => $student_2,
+        'student_3' => $student_3,
+        'student_4' => $student_4,
+        'student_5' => $student_5,
            // Player 1 attempts
-           'p1_attempt1' => $request->p1_attempt1,
-           'p1_attempt2' => $request->p1_attempt2,
-           'p1_attempt3' => $request->p1_attempt3,
-           'p1_average' => $p1_average,
+           's1_attempt1' => $request->s1_attempt1,
+           's1_attempt2' => $request->s1_attempt2,
+           's1_average' => $s1_average,
+            's1_remarks' => $s1_remarks,
+           
+           
            // Player 2 attempts
-           'p2_attempt1' => $request->p2_attempt1,
-           'p2_attempt2' => $request->p2_attempt2,
-           'p2_attempt3' => $request->p2_attempt3,
-           'p2_average' => $p2_average,
+           's2_attempt1' => $request->s2_attempt1,
+           's2_attempt2' => $request->s2_attempt2,
+           's2_average' => $s2_average,
+           's2_remarks' => $s2_remarks,
+           
            // Player 3 attempts
-           'p3_attempt1' => $request->p3_attempt1,
-           'p3_attempt2' => $request->p3_attempt2,
-           'p3_attempt3' => $request->p3_attempt3,
-           'p3_average' => $p3_average
+           's3_attempt1' => $request->s3_attempt1,
+           's3_attempt2' => $request->s3_attempt2,
+           's3_remarks' => $s3_remarks,
+           's3_average' => $s3_average,
+           // Player 4 attempts
+           's4_attempt1' => $request->s4_attempt1,
+           's4_attempt2' => $request->s4_attempt2,
+           's4_average' => $s4_average,
+           's4_remarks' => $s4_remarks,
+           
+           // Player 5 attempts
+           's5_attempt1' => $request->s4_attempt1,
+           's5_attempt2' => $request->s4_attempt2,
+           's5_average' => $s5_average,
+           's5_remarks' => $s5_remarks,
+           
        ]);
    }
 }
